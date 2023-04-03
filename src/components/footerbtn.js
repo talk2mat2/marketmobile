b5714a3814afaf6d5c25b7947675eb4e09362f1f
Martins Chukwuma <talk2mat6@gmail.com>
2023-04-03 12:35:33 +0100

    init

--- src/components/footerbtn.js
+++ src/components/footerbtn.js
@@ -4,7 +4,7 @@ const FooterBtn = ({title="",children}) => {
   return (
     <button style={{ background:"inherit" }} className="d-flex flex-column align-items-center p-1 border-0">
       <div className="foot-btn-bg d-flex align-items-center justify-content-center">{children}</div>
-      <h6 className=" footerTitle mt-2">{title}</h6>
+      <h6 className="footerTitle mt-2">{title}</h6>
     </button>
   );
 };
